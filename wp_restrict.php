d40d3f2029ce7d45eb17379fa0ec0a93aec29378
shaune <info@shaunewest.com>
2013-04-17 22:32:32 +1000

    Create wp_restrict.php
    Initial

---
+++ wp_restrict.php
@@ -0,0 +1,28 @@
+<?php
+  /*
+	Plugin Name: Restrict
+	Plugin URI: http://shaunewest.com/
+	Description: Restrict Pages
+	Version: 1.00
+	Author: Shaune West
+	Author URI: http://shaunewest.com
+	*/
+	add_shortcode('restrict', 'restrict_shortcode');
+	function restrict_shortcode( $atts, $content = null ) {
+		$defaults = array(
+			'role' => 'read'
+		);
+		extract( shortcode_atts( $defaults, $atts ));
+		$roles =  explode(',', $role); 
+		if (is_user_logged_in()){
+			if(!empty($roles)) {
+				foreach($roles as $role) {
+					if(current_user_can($role))
+						return "".$content."";
+				}
+			}
+		}else{
+			return "<p>Log in to view content</p>";
+		}
+	}
+?>
