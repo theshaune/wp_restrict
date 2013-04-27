27b815bc5e95a5b05263a2d300f8022cef031f07
shaune <info@shaunewest.com>
2013-04-27 12:59:38 +1000

    Version 1.1.03
    An easy way to limit what data is shown to users with a simple a shortcode. Any content placed inside the shortcode will be restricted to logged in users only with the correct capabilities.

--- wp_restrict.php
+++ wp_restrict.php
@@ -1,16 +1,18 @@
 <?php
 /*
-Plugin Name: Restrict
-Plugin URI: http://shaunewest.com/
-Description: Restrict Pages
-Version: 1.00
+Plugin Name: Restrict lite
+Plugin URI: http://wordpress.org/extend/plugins/restrict-lite/
+Description: An easy way to limit what data is shown to users with a simple a shortcode. Any content placed inside the shortcode will be restricted to logged in users only with the correct capabilities.
+Version: 1.1.0.3
 Author: Shaune West
 Author URI: http://shaunewest.com
+License: GPL2
 */
 add_shortcode('restrict', 'restrict_shortcode');
 function restrict_shortcode($atts, $content = null) {
 	$defaults = array(
-		'role' => 'read'
+		'role' => '',
+		'capability' => ''
 	);
 	extract(shortcode_atts($defaults, $atts));
 	$roles = explode(',', $role); 
@@ -20,9 +22,11 @@ function restrict_shortcode($atts, $content = null) {
 				if (current_user_can($role))
 					return $content;
 			}
+		}elseif (!empty($capability)) {
+			foreach($capability as $capability) {
+				if (current_user_can($capability))
+					return $content;
+			}
 		}
-	} else {
-		return "<p>Log in to view content</p>";
 	}
 }
-
